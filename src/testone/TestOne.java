b6be186e6c6a3b6a06e52e25c0802f7eb1f0bb45
ST10279031-NikitaCorreiaFreire <ST10279031@vcconnect.edu.za>
2024-03-19 13:32:59 +0200

    comitting to master branch
    test code

---
+++ src/testone/TestOne.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testone;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author nikitacorreiafreire
+ */
+public class TestOne {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        JOptionPane.showInputDialog(null,"Is this working?");
+    }
+    
+}
